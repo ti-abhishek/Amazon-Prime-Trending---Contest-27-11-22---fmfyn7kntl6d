99fc6afe58acffa83ba21c0357f34fa5165b6198
ti-abhishek <ti.abhishek15@gmail.com>
2022-11-27 20:50:31 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,20 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <>
+    <h1 id="heading">Amazon Prime: Trending</h1>
+      <div>
+        <div>
+          <h1 data-testid="title">{slides[slideIndex].title}</h1>
+          <p data-testid="text">{slides[slideIndex].text}</p>
+        </div>
+        
+        <button data-testid="button-prev" onClick={handlePrevClick} disabled={prevFlag}>Prev</button>
+        <button data-testid="button-next" onClick={handleNextClick} disabled={nextFlag}>Next</button>
+        <button data-testid="button-restart" onClick={handleRestartClick} disabled={restartFlag}>Restart</button>
+    
+      </div>
+    </>
   )
 }
 
